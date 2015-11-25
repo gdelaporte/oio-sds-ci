f140bdb34b4808299fb79f760be752f9e41e0801
Guillaume Delaporte <guillaume.delaporte@openio.io>
2015-11-25 16:09:20 +0100

    Launch tests only on one type of instance

--- oio-sds-ci.sh
+++ oio-sds-ci.sh
@@ -1,8 +1,15 @@
-export OIO_CDI_DIR="/tmp"
+#!/bin/bash
+
+# Define the packager installion function
+# For Ubuntu only in this first version : apt-get
+function pkg_install () { sudo apt-get -y install $@ ; }
+
 sudo apt-get update
-sudo apt-get install -y git
-sudo useradd openio -m -d /home/openio
-su - openio -c "git clone https://github.com/GuillaumeDelaporte/oio-sds-ci ${OIO_CDI_DIR}/oio-sds-ci"
-sudo ${OIO_CDI_DIR}/oio-sds-ci/build.sh
-sudo ${OIO_CDI_DIR}/oio-sds-ci/setup.sh
-su - openio -c ${OIO_CDI_DIR}/oio-sds-ci/run_tests.sh
+pkg_install git
+
+export OIO_HOME="/home/openio"
+sudo useradd openio -m -d ${OIO_HOME}
+sudo su - openio -c "git clone https://github.com/GuillaumeDelaporte/oio-sds-ci ${OIO_HOME}/oio-sds-ci"
+sudo ${OIO_HOME}/oio-sds-ci/build.sh
+sudo ${OIO_HOME}/oio-sds-ci/setup.sh
+sudo su - openio -c ${OIO_HOME}/oio-sds-ci/run_tests.sh ${TESTS_ARGS}
--- run_tests.sh
+++ run_tests.sh
@@ -19,15 +19,33 @@ git clone https://github.com/open-io/oio-sds
 sudo pip install nose-htmloutput
 
 # Launch a minimalist instance of oio-sds (no sqlx, no zookeeper, repli x1)
-export NOSE_ARGS="--with-xunit --xunit-file=${TMPDIR}/nosestests-single.xml --with-html --html-file=${TMPDIR}/nosestests-single.html"
-oio-reset.sh -S SINGLE -X sqlx -X zookeeper -R 1 -B 1
-cd ${TMPDIR}/oio-sds/python
-tox -e func
+function run_single_instance () {
+	export NOSE_ARGS="--with-xunit --xunit-file=${TMPDIR}/nosestests-single.xml --with-html --html-file=${TMPDIR}/nosestests-single.html"
+	oio-reset.sh -S SINGLE -X sqlx -X zookeeper -R 1 -B 1
+	cd ${TMPDIR}/oio-sds/python
+	tox -e func
+}
 
 # Launch functional test
-export ADD_META1=3
-export ADD_META2=3
-export NOSE_ARGS="--with-xunit --xunit-file=${TMPDIR}/nosestests-repli3.xml --with-html --html-file=${TMPDIR}/nosestests-repli3.html"
-oio-reset.sh -S SINGLE -X sqlx -R 1 -B 1 -D 1 -S THREECOPIES
-cd ${TMPDIR}/oio-sds/python
-tox -e func
+function run_repli3_instance () {
+	export ADD_META1=3
+	export ADD_META2=3
+	export NOSE_ARGS="--with-xunit --xunit-file=${TMPDIR}/nosestests-repli3.xml --with-html --html-file=${TMPDIR}/nosestests-repli3.html"
+	oio-reset.sh -S SINGLE -X sqlx -R 1 -B 1 -D 1 -S THREECOPIES
+	cd ${TMPDIR}/oio-sds/python
+	tox -e func
+}
+
+case "$1" in
+        single)
+            run_single_instance
+            ;;
+
+        replix3)
+            run_repli3_instance
+            ;;
+
+        *)
+ 	    run_single_instance
+	    run_repli3_instance
+esac
