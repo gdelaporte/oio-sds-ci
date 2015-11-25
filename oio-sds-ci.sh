ecf15a3d1bac5acd89bc82c64e3ad992ba4b6fc6
Guillaume Delaporte <guillaume.delaporte@openio.io>
2015-11-25 17:51:45 +0100

    Fix issue with arguments

--- oio-sds-ci.sh
+++ oio-sds-ci.sh
@@ -14,4 +14,4 @@ sudo useradd openio -m -d ${OIO_HOME}
 sudo su - openio -c "git clone https://github.com/GuillaumeDelaporte/oio-sds-ci ${OIO_HOME}/oio-sds-ci"
 sudo ${OIO_HOME}/oio-sds-ci/build.sh
 sudo ${OIO_HOME}/oio-sds-ci/setup.sh
-sudo su - openio -c ${OIO_HOME}/oio-sds-ci/run_tests.sh ${TESTS_ARGS}
+sudo su - openio -c "${OIO_HOME}/oio-sds-ci/run_tests.sh ${TESTS_ARGS}"
