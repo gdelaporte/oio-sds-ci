f37d5fe4c5f881a0697bd1059e60b6a5896f7d55
Guillaume Delaporte <guillaume.delaporte@openio.io>
2015-12-15 16:15:06 +0100

    Add repository with latest version of redis

--- setup.sh
+++ setup.sh
@@ -8,6 +8,10 @@ function pkg_install () { sudo apt-get -y install $@ ; }
 pkg_install zookeeper python-zookeeper
 sudo /usr/share/zookeeper/bin/zkServer.sh start
 
+# Add new repo with latest version of redis
+pkg_install python-software-properties
+sudo add-apt-repository -y ppa:rwky/redis
+sudo apt-get update
 # Install redis
 pkg_install redis-server
 
