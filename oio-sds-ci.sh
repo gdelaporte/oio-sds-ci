da66cde6ce877aa8ea4f01c55778be95874554aa
Guillaume Delaporte <guillaume.delaporte@openio.io>
2015-12-16 15:07:56 +0100

    Fix

--- oio-sds-ci.sh
+++ oio-sds-ci.sh
@@ -3,21 +3,21 @@
 # Usage
 while getopts ":r:g:p:b:c:x" opt; do
 	case $opt in
-		r) REPLICATION_LEVEL="${OPTARG}" ;;
-		g) GIT_COMMIT="${OPTARG}" ;;
-		p) PULL_ID="${OPTARG}" ;;
-		b) BRANCH="${OPTARG}" ;;
-		c) COMMIT_ID="${OPTARG}" ;;
+		r) REPLICATION_LEVEL="-r ${OPTARG}" ;;
+		g) GIT_COMMIT="-g ${OPTARG}" ;;
+		p) PULL_ID="-p ${OPTARG}" ;;
+		b) BRANCH="-b ${OPTARG}" ;;
+		c) COMMIT_ID="-c ${OPTARG}" ;;
 		\?) ;;
 	esac
 done
 
 echo "$0" \
-	"-r \"${REPLICATION_LEVEL}\"" \
-	"-g \"${GIT_COMMIT}\"" \
-	"-p \"${PULL_ID}\"" \
-	"-b \"${BRANCH}\"" \
-	"-c \"${COMMIT_ID}\"" \
+	"${REPLICATION_LEVEL}" \
+	"${GIT_COMMIT}" \
+	"${PULL_ID}" \
+	"${BRANCH}" \
+	"${COMMIT_ID}" \
 
 # Define the packager installion function
 # For Ubuntu only in this first version : apt-get
@@ -30,6 +30,6 @@ export OIO_HOME="/home/openio"
 sudo useradd openio -m -d ${OIO_HOME}
 sudo echo "openio ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/openio
 sudo su - openio -c "git clone https://github.com/GuillaumeDelaporte/oio-sds-ci ${OIO_HOME}/oio-sds-ci"
-sudo ${OIO_HOME}/oio-sds-ci/build.sh -p ${PULL_ID} -b ${BRANCH} -c ${COMMIT_ID}
+sudo ${OIO_HOME}/oio-sds-ci/build.sh ${PULL_ID} ${BRANCH} ${COMMIT_ID}
 sudo ${OIO_HOME}/oio-sds-ci/setup.sh
-sudo su - openio -c "${OIO_HOME}/oio-sds-ci/run_tests.sh -r ${REPLICATION_LEVEL} -p ${PULL_ID} -b ${BRANCH} -c ${COMMIT_ID}"
+sudo su - openio -c "${OIO_HOME}/oio-sds-ci/run_tests.sh ${REPLICATION_LEVEL} ${PULL_ID} ${BRANCH} ${COMMIT_ID}"
